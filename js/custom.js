6bbdbe82afba48453089c5f485a7875f6e644981
Mark Pavlovski <markpavlovski@gmail.com>
2018-02-25 13:12:06 -0800

    added delay for the bottom left menu

--- js/custom.js
+++ js/custom.js
@@ -1,18 +1,18 @@
 $(document).ready(() => {
-  $(".content-item-y").click(goMain);
-  $(".email").click(goContent);
-  $(".main-grid-item").click(goContent);
-  $('.fixed-action-btn').openFAB();
+  $(".content-item-y").click(goMain)
+  $(".email").click(goContent)
+  $(".main-grid-item").click(goContent)
+  setTimeout(() => $('.fixed-action-btn').openFAB(), 750)
 });
 
 
 function goMain() {
-  $( ".content-grid-container" ).css( "display", "none" );
-  $( ".main-grid-container" ).css( "display", "grid" );
+  $( ".content-grid-container" ).css( "display", "none" )
+  $( ".main-grid-container" ).css( "display", "grid" )
 }
 
 
 function goContent() {
-  $( ".main-grid-container" ).css( "display", "none" );
-  $( ".content-grid-container" ).css( "display", "grid" );
+  $( ".main-grid-container" ).css( "display", "none" )
+  $( ".content-grid-container" ).css( "display", "grid" )
 }
