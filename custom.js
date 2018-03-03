8e108628a7a4a04ef00013846e7cac97a2165307
Mark Pavlovski <markpavlovski@gmail.com>
2018-03-03 08:49:34 -0800

    figured out why the navbar i icons are not changing

--- custom.js
+++ custom.js
@@ -1,6 +1,6 @@
-let yoff = 0.0
-let yoff2 = 0.0
+// DRAW ON CANVAS
 
+let yoff = 0.0
 function setup() {
   const cnv = createCanvas(window.innerWidth, window.innerHeight)
   cnv.parent(document.querySelector('header'))
@@ -14,22 +14,12 @@ fill(255);
 // We are going to draw a polygon out of the wave points
 beginShape();
 
-var xoff = 0;       // Option #1: 2D Noise
-// var xoff = yoff; // Option #2: 1D Noise
+var xoff = 0;
 
 // Iterate over horizontal pixels
 for (var x = 0; x <= width; x += 1) {
-  // Calculate a y value according to noise, map to
-
-  // Option #1: 2D Noise
   var y = map(noise(xoff, yoff), 0.5, 1, window.innerHeight*.85,window.innerHeight*.95);
-
-  // Option #2: 1D Noise
-  // var y = map(noise(xoff), 0, 1, 200,300);
-
-  // Set the vertex
   vertex(x, y);
-  // Increment x dimension for noise
   xoff += 0.001;
 }
 // increment y dimension for noise
@@ -38,37 +28,24 @@ vertex(width, height);
 vertex(0, height);
 endShape(CLOSE);
 
-
-
-//
-// background('rgba(100%,100%,70%,0.0)');
-// stroke('rgba(100%,20%,70%,0)');
-// strokeWeight(10)
-// fill(51);
-// // We are going to draw a polygon out of the wave points
-// beginShape();
-//
-// var xoff = 0;       // Option #1: 2D Noise
-// // var xoff = yoff; // Option #2: 1D Noise
-//
-// // Iterate over horizontal pixels
-// for (var x = 0; x <= width; x += 1) {
-//   // Calculate a y value according to noise, map to
-//
-//   // Option #1: 2D Noise
-//   var y = map(noise(xoff, yoff), 0.5, 1, window.innerHeight*.95,window.innerHeight);
-//
-//   // Option #2: 1D Noise
-//   // var y = map(noise(xoff), 0, 1, 200,300);
-//
-//   // Set the vertex
-//   vertex(x, y);
-//   // Increment x dimension for noise
-//   xoff += 0.1;
-// }
-// // increment y dimension for noise
-// yoff += 0.001;
-// vertex(width, height);
-// vertex(0, height);
-// endShape(CLOSE);
 }
+
+
+//
+
+
+document.addEventListener("scroll",()=>{
+
+  const main = document.querySelector("main")
+  const topLeft = document.querySelector("#top-left")
+  const allI = document.querySelector("i")
+
+  if (window.pageYOffset > main.offsetTop*.81){
+    topLeft.classList.add("dark-font")
+    allI.classList.add("dark-font")
+  } else {
+    topLeft.classList.remove("dark-font")
+    allI.classList.remove("dark-font")
+  }
+  
+})
