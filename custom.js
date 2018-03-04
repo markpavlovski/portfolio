71561ac2c914ebf08fbe46e53682f0bf92d73ca1
Mark Pavlovski <markpavlovski@gmail.com>
2018-03-03 22:53:19 -0800

    added shade

--- custom.js
+++ custom.js
@@ -71,3 +71,23 @@ document.addEventListener("scroll",()=>{
   }
 
 })
+
+// TILTING
+
+const tilt = 5
+const shade = 15
+const calculator = document.querySelector(".card")
+
+document.addEventListener("mousemove",(event)=>{
+  center = {
+    x: window.innerWidth/2,
+    y: window.innerHeight/2
+  }
+  delta = {
+    x: (event.x - center.x)/(window.innerWidth - center.x),
+    y: (event.y - center.y)/(window.innerHeight - center.y)
+  }
+  calculator.style.transform = `perspective( 600px ) rotateY( ${delta.x * tilt}deg ) rotateX( ${-delta.y * tilt}deg )`;
+  /* offset-x | offset-y | blur-radius | spread-radius | color */
+  calculator.style.boxShadow = `${-delta.x * shade + 0}px ${-delta.y * shade + 0}px ${40}px ${0}px rgba(0, 0, 0, 0.2)`
+})
