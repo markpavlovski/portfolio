d3a999e8a277becb886e0a82fb70a009b05435e7
Mark Pavlovski <markpavlovski@gmail.com>
2018-03-04 22:57:29 -0800

    implemented card hiding as the page scrolls up

--- custom.js
+++ custom.js
@@ -10,65 +10,81 @@ function setup() {
 }
 
 function draw() {
-// background(79,139,204,);
-background(51,51,51);
-stroke('rgba(50%,100%,70%,0.0)');
-strokeWeight(1)
-fill(255);
-// We are going to draw a polygon out of the wave points
-beginShape();
+  // background(79,139,204,);
+  background(51, 51, 51);
+  stroke('rgba(50%,100%,70%,0.0)');
+  strokeWeight(1)
+  fill(255);
+  // We are going to draw a polygon out of the wave points
+  beginShape();
 
-var xoff = 0;
+  var xoff = 0;
 
-// Iterate over horizontal pixels
-for (var x = 0; x <= width; x += 1) {
-  var y = map(noise(xoff, yoff), 0.5, 1, window.innerHeight*.80,window.innerHeight*.90);
-  vertex(x, y);
-  xoff += 0.001;
-}
-// increment y dimension for noise
-yoff += 0.002;
-vertex(width, height);
-vertex(0, height);
-endShape(CLOSE);
+  // Iterate over horizontal pixels
+  for (var x = 0; x <= width; x += 1) {
+    var y = map(noise(xoff, yoff), 0.5, 1, window.innerHeight * .80, window.innerHeight * .90);
+    vertex(x, y);
+    xoff += 0.001;
+  }
+  // increment y dimension for noise
+  yoff += 0.002;
+  vertex(width, height);
+  vertex(0, height);
+  endShape(CLOSE);
 
 }
 
 // RESIZE CANVAS ON WINDOW RESIZE
 
-window.addEventListener("resize",()=>{
+window.addEventListener("resize", () => {
   const canvas = document.querySelector("header > canvas")
   canvas.style = `width: ${innerWidth}px; height: ${innerHeight}px;`
 })
 
 //
 
-document.querySelector("#title").addEventListener("click",()=>{
-  console.log("nice")
+document.querySelector("#title").addEventListener("click", () => {
   document.querySelector('#title')
-          .scrollIntoView({behavior: "smooth", block: "start"});
+    .scrollIntoView({
+      behavior: "smooth",
+      block: "start"
+    });
 })
 
-// Change navbar color
+// Change navbar color & add disappearing cards
 
-document.addEventListener("scroll",()=>{
+document.addEventListener("scroll", () => {
 
   const main = document.querySelector("main")
   const topLeft = document.querySelector("#top-left")
-  let icons = document.querySelectorAll("#top-right i")
+  const icons = document.querySelectorAll("#top-right i")
+  const cards = document.querySelectorAll(".card")
+  const titleBox = document.querySelector("#title").getBoundingClientRect()
 
-  if (window.pageYOffset > main.offsetTop*.81){
+  // toggle header color
+  if (window.pageYOffset > main.offsetTop * .81) {
     topLeft.classList.add("dark-font")
-    for(let i=0; i < icons.length; i++){
+    for (let i = 0; i < icons.length; i++) {
       icons[i].classList.add("dark-font")
     }
-
   } else {
     topLeft.classList.remove("dark-font")
-    for(let i=0; i < icons.length; i++){
+    for (let i = 0; i < icons.length; i++) {
       icons[i].classList.remove("dark-font")
     }
   }
+
+  for (let i = 0; i < cards.length; i++) {
+    if (titleBox.y + titleBox.height > cards[i].getBoundingClientRect().y) {
+      cards[i].classList.add("invisible")
+    } else {
+      cards[i].classList.remove("invisible")
+
+    }
+  }
+
+
+
 
 })
 
@@ -76,20 +92,19 @@ document.addEventListener("scroll",()=>{
 const favoritesJSON = localStorage.getItem("favorites")
 const favorites = favoritesJSON ? JSON.parse(favoritesJSON) : {}
 const stars = document.querySelectorAll(".card i.fa-star")
-for (let i=0; i<stars.length;i++){
-  if (favorites[stars[i].closest(".card").id]){
+for (let i = 0; i < stars.length; i++) {
+  if (favorites[stars[i].closest(".card").id]) {
     stars[i].classList.toggle("fas")
     stars[i].classList.toggle("i-fav")
   }
 }
 
-document.querySelector("container").addEventListener("click",(event)=>{
-  if (event.target.classList.contains("fa-star")){
+document.querySelector("container").addEventListener("click", (event) => {
+  if (event.target.classList.contains("fa-star")) {
     const id = event.target.closest(".card").id;
     favorites[id] = !favorites[id]
     event.target.classList.toggle("fas")
     event.target.classList.toggle("i-fav")
-    console.log(favorites[id])
-    localStorage.setItem("favorites",JSON.stringify(favorites))
+    localStorage.setItem("favorites", JSON.stringify(favorites))
   }
 })
